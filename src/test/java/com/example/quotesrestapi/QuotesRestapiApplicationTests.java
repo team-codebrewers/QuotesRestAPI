fb845f72d0f4035a87821927b2a222bdff6a7eac
CoderAbhinav <abhinavrb.a02@gmail.com>
2021-11-21 11:18:46 +0530

    Added tests to API

--- src/test/java/com/example/quotesrestapi/QuotesRestapiApplicationTests.java
+++ src/test/java/com/example/quotesrestapi/QuotesRestapiApplicationTests.java
@@ -2,12 +2,20 @@ package com.example.quotesrestapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class QuotesRestapiApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void checkHello(){
+        QuotesRestapiApplication home = new QuotesRestapiApplication();
+        String result = home.hello();
+        assertEquals(result, "Hello World!");
     }
 
 }
