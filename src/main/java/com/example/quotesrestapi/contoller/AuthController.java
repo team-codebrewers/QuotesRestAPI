c82afe298be0a418b23c7f883896a6e1e94f0895
Shardul_Joshi <sharduljoshi9@gmail.com>
2021-11-29 21:13:05 +0530

    Added toking issue api call.

--- src/main/java/com/example/quotesrestapi/contoller/AuthController.java
+++ src/main/java/com/example/quotesrestapi/contoller/AuthController.java
@@ -1,19 +1,33 @@
 package com.example.quotesrestapi.contoller;
 
+import com.example.quotesrestapi.model.SuperUser;
 import com.example.quotesrestapi.model.User;
+import com.example.quotesrestapi.services.TokenGenerator;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
 public class AuthController {
+
     @PostMapping("/issue")
-    public User issueToken(){
-        return null;
+    public User authenticateUser (@RequestHeader (name="x-api-key")String token,@RequestParam(name="email")String email,@RequestParam(name="privilege")String privilege){
+
+            try{
+                SuperUser superUser=new SuperUser(token);
+                 User user=superUser.registerUser(email,privilege);
+                    return user;
+
+                }catch(Exception e){
+                    System.out.println("Not a Super User");
+
+                }
+
+
+
+    return null;
     }
 
-    @DeleteMapping("/remove")
-    public void removeToken(){
 
-    }
+
 
 }
--- src/main/java/com/example/quotesrestapi/model/Quote.java
+++ src/main/java/com/example/quotesrestapi/model/Quote.java
@@ -19,6 +19,38 @@ public class Quote {
         this.tags = List.of(tags.split("-"));
     }
 
+    public String getQuote() {
+        return quote;
+    }
+
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+
+    @Override
+    public String toString() {
+        return "Quote{" +
+                "quote='" + quote + '\'' +
+                ", author='" + author + '\'' +
+                ", tags=" + tags +
+                '}';
+    }
 }
 
 // Quote{quote='TEXT', author='TEXT'}
