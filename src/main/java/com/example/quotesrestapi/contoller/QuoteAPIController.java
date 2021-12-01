d08a81979b216cb8c46ed86daccb1ccb076f7b10
Aryan Shroff <aryanshroff21@gmail.com>
2021-12-01 10:56:29 +0530

    completed_todo

--- src/main/java/com/example/quotesrestapi/contoller/QuoteAPIController.java
+++ src/main/java/com/example/quotesrestapi/contoller/QuoteAPIController.java
@@ -4,36 +4,144 @@ import com.example.quotesrestapi.model.Quote;
 import com.example.quotesrestapi.services.DatabaseService;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 @RestController
 @RequestMapping("/api/quotes")
 public class QuoteAPIController {
     @GetMapping("/")
     public List<Quote> getRandomQuote(@RequestParam(name = "count", defaultValue = "1")int count){
-        // TODO: @Aryan write a code to send random quote from database (Or given number of quotes randomly)
-        System.out.println(count);
+
+        String query = "SELECT * FROM quotes_dataset ";
+        if(count <= 0) count = 1;
+        else if(count > 50) count = 50;
+        try{
+            DatabaseService db = new DatabaseService();
+            List<Quote> quoteList = db.getQuoteQuery(query);
+            db.closeConnection();
+            List<Quote> rev = new ArrayList<>();
+            if(quoteList.size() == 0){
+                return null;
+            }
+
+            if(quoteList.size() < count) {
+                count = quoteList.size();
+            }
+
+            for(int i = 0; i < count ; i++){
+                int randomIndex = new Random().nextInt(quoteList.size());
+                rev.add(quoteList.get(randomIndex));
+                quoteList.remove(randomIndex);
+            }
+
+            return rev;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return null;
+
     }
 
     @GetMapping("/search")
     public List<Quote> searchKeyword(@RequestParam(name = "query", defaultValue = "")String keyWord,@RequestParam(name = "count", defaultValue = "10")int count){
-        // TODO: @Aryan write code to return quotes containing given keyword
-        System.out.println(keyWord);
+
+        String query = "SELECT * FROM quotes_dataset WHERE quote LIKE '%"+keyWord+"%' OR author LIKE '%"+keyWord+"%' OR tags LIKE '%"+keyWord+"%'";
+        if(count <= 0) count = 1;
+        else if(count > 50) count = 50;
+        try{
+            DatabaseService db = new DatabaseService();
+            List<Quote> quoteList = db.getQuoteQuery(query);
+            db.closeConnection();
+            List<Quote> rev = new ArrayList<>();
+            if(quoteList.size() == 0){
+                return null;
+            }
+
+            if(quoteList.size() < count) {
+                count = quoteList.size();
+            }
+
+            for(int i = 0; i < count ; i++){
+                int randomIndex = new Random().nextInt(quoteList.size());
+                rev.add(quoteList.get(randomIndex));
+                quoteList.remove(randomIndex);
+            }
+
+            return rev;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
 
     @GetMapping("/author/{authorName}")
     public List<Quote> getQuotesFromAuthor(@PathVariable String authorName,@RequestParam(name = "count", defaultValue = "10")int count){
-        // TODO: @Samruddhi write code to return quotes with given author
-        System.out.println(authorName);
+
+        String query = "SELECT * FROM quotes_dataset WHERE author LIKE '%"+ authorName+"%'";
+        if(count <= 0) count = 1;
+        else if(count > 50) count = 50;
+        try{
+            DatabaseService db = new DatabaseService();
+            List<Quote> quoteList = db.getQuoteQuery(query);
+            db.closeConnection();
+            List<Quote> rev = new ArrayList<>();
+            if(quoteList.size() == 0){
+                return null;
+            }
+
+            if(quoteList.size() < count) {
+                count = quoteList.size();
+            }
+
+            for(int i = 0; i < count ; i++){
+                int randomIndex = new Random().nextInt(quoteList.size());
+                rev.add(quoteList.get(randomIndex));
+                quoteList.remove(randomIndex);
+            }
+
+            return rev;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
 
     @GetMapping("/tag/{tag}")
     public List<Quote> getQuotesByTag(@PathVariable String tag,@RequestParam(name = "count", defaultValue = "10")int count){
-        // TODO: @Samruddhi write code to return quotes with given tag
-        System.out.println(tag);
+
+        String query = "SELECT * FROM quotes_dataset WHERE tag LIKE '%"+ tag+"%'";
+        if(count <= 0) count = 1;
+        else if(count > 50) count = 50;
+        try{
+            DatabaseService db = new DatabaseService();
+            List<Quote> quoteList = db.getQuoteQuery(query);
+            db.closeConnection();
+            List<Quote> rev = new ArrayList<>();
+            if(quoteList.size() == 0){
+                return null;
+            }
+
+            if(quoteList.size() < count) {
+                count = quoteList.size();
+            }
+
+            for(int i = 0; i < count ; i++){
+                int randomIndex = new Random().nextInt(quoteList.size());
+                rev.add(quoteList.get(randomIndex));
+                quoteList.remove(randomIndex);
+            }
+
+            return rev;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
+
 }
