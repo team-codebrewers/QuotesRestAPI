408bc8fd3f7044c0e3bf6d71b2fafe484c24d5a7
Abhinav Belhekar <73335766+CoderAbhinav@users.noreply.github.com>
2021-12-02 10:56:20 +0530

    Updated User Class

--- src/test/java/com/example/quotesrestapi/QuoteTests.java
+++ src/test/java/com/example/quotesrestapi/QuoteTests.java
@@ -4,6 +4,9 @@ import com.example.quotesrestapi.model.Quote;
 import com.example.quotesrestapi.quoteservice.QuoteService;
 import com.example.quotesrestapi.quoteservice.QuoteServiceImpl;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class QuoteTests {
@@ -14,20 +17,14 @@ public class QuoteTests {
     @Test
     void testConstructor(){
         Quote testQuote = new Quote("TEXT", "TEXT", "Hello-World");
-//        assertEquals( "Quote{quote='TEXT', author='TEXT', tags=[Hello, World]}",testQuote.toString());
-//        assertEquals("TEXT",testQuote.getAuthor());
+        assertEquals( "Quote{quote='TEXT', author='TEXT', tags=[Hello, World]}",testQuote.toString());
     }
 
-    @Test
-    void toStringTest(){
-        Quote testQuote = new Quote("TEXT", "TEXT", "Hello-World");
-//        assertEquals( "Quote{quote='TEXT', author='TEXT', tags=[Hello, World]}",testQuote.toString());
-    }
 
     @Test
     void getterTest(){
         Quote testQuote = new Quote("TEXT", "TEXT", "Hello-World");
-//        assertEquals("TEXT",testQuote.getAuthor());
+        assertEquals("TEXT",testQuote.getAuthor());
     }
 
 }
